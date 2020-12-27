dc8d219eec1f8a13db77f1a5f37eeb4e5a63dcfb
redmundnacario <redmund.nacario@gmail.com>
2020-12-28 05:20:34 +0800

    image minification, cache and gulp default cmd

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ const browserSync = require("browser-sync").create();
 const cssnano = require("gulp-cssnano");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+
 
 // Sass
 
@@ -38,6 +42,8 @@ gulp.task("sass", function(done) {
 gulp.task("javascript", function(done) {
     return gulp
         .src("./src/js/**/*.js")
+        // .src(["./src/js/alert.js", "./src/js/project.js"])
+        // .pipe(concat("project.js"))
         .pipe(uglify()) //minify javascript
         .pipe(
             rename({
@@ -46,7 +52,18 @@ gulp.task("javascript", function(done) {
         )
         .pipe(gulp.dest("./dist/js"));
     done();
-  });
+});
+
+// Images optimization
+
+gulp.task("imagemin", function(done) {
+    return (
+      gulp.src("./src/img/**/*.+(png|jpg|gif|svg)")
+      .pipe(cache(imagemin()))
+      .pipe(gulp.dest("./dist/img/"))
+    )
+    done();
+})
 
 // Watch task with BrowserSync
 
@@ -63,8 +80,23 @@ gulp.task("watch", function() {
             [
                 "./src/sass/**/*.scss",
                 "**/*.html",
-                "./src/js/**/*.js"
-            ], gulp.series(["sass", "javascript"])
+                "./src/js/**/*.js",
+                "./src/img/**/*.+(png|jpg|gif|svg)"
+            ], 
+            gulp.series(["sass", "javascript", "imagemin"])
         )
         .on("change", browserSync.reload);
 });
+
+
+// Clear cache
+
+gulp.task("clear-cache", function(done) {
+    return cache.clearAll(done);
+  });
+  
+  // Gulp default command
+  
+gulp.task("default", gulp.series(["watch"]));
+  
+  
